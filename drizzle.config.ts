0879e3260df3e63775442a8ccfa6863e055d6e6d
ConradoFrasson <conradox12@gmail.com>
2025-08-13 16:56:44 -0300

    Remove package.json file and its associated dependencies and scripts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from "drizzle-kit";
 
 if (!process.env.DATABASE_URL) {
@@ -13,19 +12,3 @@ export default defineConfig({
     url: process.env.DATABASE_URL,
   },
 });
-=======
-import { defineConfig } from "drizzle-kit";
-
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL, ensure the database is provisioned");
-}
-
-export default defineConfig({
-  out: "./migrations",
-  schema: "./shared/schema.ts",
-  dialect: "postgresql",
-  dbCredentials: {
-    url: process.env.DATABASE_URL,
-  },
-});
->>>>>>> da2b8811db024d8bd3a8f8d6773c3fee914a8231
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "dotenv/config";
 import "./config/db.js";
 import e from "express";
@@ -10,17 +9,4 @@ app.use(e.json());
 
 app.use("/consortium", consortiumCardsRoute);
 
-=======
-import "dotenv/config";
-import "./config/db.js";
-import e from "express";
-import consortiumCardsRoute from "./routes/consortiumCardsRoute.js";
-
-const app = e();
-
-app.use(e.json());
-
-app.use("/consortium", consortiumCardsRoute);
-
->>>>>>> da2b8811db024d8bd3a8f8d6773c3fee914a8231
 app.listen(process.env.PORT, () => console.log("App running!"));
