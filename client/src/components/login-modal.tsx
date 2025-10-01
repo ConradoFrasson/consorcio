cb59f70643627dc179808121d75bf9c74b8cac5c
ConradoFrasson <conradox12@gmail.com>
2025-10-01 20:17:25 -0300

    correcao

--- client/src/components/login-modal.tsx
+++ client/src/components/login-modal.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import * as React from "react";
+import { useState } from "react";
 import { useMutation } from "@tanstack/react-query";
 import { useLocation } from "wouter";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../components/ui/dialog";
