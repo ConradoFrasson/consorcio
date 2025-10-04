7346c24ad6b8116a39416857b60e33e9b08184ef
ConradoFrasson <conradox12@gmail.com>
2025-10-04 09:33:35 -0300

    correcao

--- server/drizzle.ts
+++ server/drizzle.ts
@@ -1,5 +1,6 @@
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import * as schema from "../shared/schema";
+import Database from "better-sqlite3";
 
 const sqlite = new Database("./sqlite.db");
 export const db = drizzle(sqlite, { schema });
